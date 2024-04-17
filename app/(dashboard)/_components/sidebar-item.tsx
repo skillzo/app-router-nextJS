991765c7acf14c3e0153856f000331bb9aaba1e0
skillzo <skillzfire@yahoo.com>
2024-04-17 19:30:38 +0100

    type erros not fixed

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
 interface SidebarItemProps {
-  icon: LucideIcon;
+  icon: any;
   label: string;
   href: string;
-};
+}
 
-export const SidebarItem = ({
-  icon: Icon,
-  label,
-  href,
-}: SidebarItemProps) => {
+export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
 
@@ -26,7 +21,7 @@ export const SidebarItem = ({
 
   const onClick = () => {
     router.push(href);
-  }
+  };
 
   return (
     <button
@@ -34,16 +29,14 @@ export const SidebarItem = ({
       type="button"
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
-        isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+        isActive &&
+          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
         <Icon
           size={22}
-          className={cn(
-            "text-slate-500",
-            isActive && "text-sky-700"
-          )}
+          className={cn("text-slate-500", isActive && "text-sky-700")}
         />
         {label}
       </div>
@@ -54,5 +47,5 @@ export const SidebarItem = ({
         )}
       />
     </button>
-  )
-}
+  );
+};
